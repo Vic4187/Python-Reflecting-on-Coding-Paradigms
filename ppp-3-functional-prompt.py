3a422ed2eed8f0900be0bee366187aa07887fee7
Victor Estrada <vic4187@gmail.com>
2024-06-18 19:32:31 -0500

    Added Functional Prompt and OOP

--- ppp-3-functional-prompt.py
+++ ppp-3-functional-prompt.py
@@ -7,4 +7,4 @@ def flatten_and_sort(array):
             arr.append(i)
     return sorted(arr)
 
-print(flatten_and_sort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]))
+print(flatten_and_sort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]))  # Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
